1a0192c140c4251f2c5741bcb88c7ff1b937a466
harry-danni <659863760@qq.com>
2018-11-21 18:14:58 +0800

    初始版本提交
    Change-Id: I6f9063215b86d4a7e8fcc2c0686b433e340c0df5
    
    Change-Id: I6f9063215b86d4a7e8fcc2c0686b433e340c0df5

---
+++ useage/src/main/java/harry/git/useage/App.java
@@ -0,0 +1,13 @@
+package harry.git.useage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
